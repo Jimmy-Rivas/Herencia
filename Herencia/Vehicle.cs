dd9a0809d4df1a2e7c972cc9ba67515ca63ee3b0
Jimmy Rivas <jimmyrivas568@gmail>
2021-09-28 16:51:30 -0600

    Herencia

---
+++ Herencia/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    class Vehicle
+    {
+        public int KilometersDrive { get; set; }
+        public string brand { get; set; }
+        public int horsePower { get; set; }
+
+        public int numberofseats { get; set; }
+
+        public void vehicle()
+        {
+
+        }
+
+        
+
+
+
+    }
+}
